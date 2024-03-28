fb85197d1f96547e16fa2ea0ee28a92281dadd32
Slugsy <slugsy@slugsy.me>
2024-03-28 15:34:46 -0700

    adding provider

--- modules/account/main.tf
+++ modules/account/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    newrelic = {
+      source  = "newrelic/newrelic"
+    }
+  }
+}
+
+# Configure the New Relic provider
+provider "newrelic" {
+  account_id = var.account_id
+  api_key = var.api_key 
+  region = "US" 
+}
+
 resource "newrelic_account_management" "foo"{
     count = var.new_account_num
     name=  element(var.new_account, count.index)
